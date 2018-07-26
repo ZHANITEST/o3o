407aca2f0fb01165df6500f93d61356309edfe01
ZHANITEST <zhanitest@hotmail.com>
2018-07-27 01:06:51 +0900

    컴포넌트 인터페이스 종류추가

--- source/o3o/IComponent.d
+++ source/o3o/IComponent.d
@@ -6,11 +6,23 @@
 
 module o3o.IComponent;
 
+import derelict.sdl2.sdl; // SDL2 레벨
+
 /**
- * IComponent 인터페이스
- *   - 게임루프 안에서 사용할 컴포넌트 규격
+ * IGeneralComponent 인터페이스
+ *   - 게임루프 안에서 사용할 일반 컴포넌트 규격
+ *   - ~는 일괄적으로 update함수가 반드시 있어야 한다
  */
-interface IComponent{
-    // ~는 일괄적으로 update함수가 반드시 있어야 한다
+interface IGeneralComponent{
     void update();
+}
+
+/**
+ * IEventComponent 인터페이스
+ *   - 게임루프 안에서 사용할 이벤트 컴포넌트 규격
+ *   -  ~는 일괄적으로 update함수를 가지며, SDL_Event인수를 받아야 한다
+ *   - SDL2의 폴링방식 때문에 함수 서명에 ref 키워드 대신 내부처리에서 & 사용
+ */
+interface IEventComponent{
+    void update(SDL_Event e);
 }
