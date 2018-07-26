3a3fe889f8701642e86b2f90d7944daf893228c7
ZHANITEST <zhanitest@hotmail.com>
2018-07-27 01:08:32 +0900

    이벤트 서비스추가에 대한 로직추가 및 수정

--- source/o3o/SGame.d
+++ source/o3o/SGame.d
@@ -9,6 +9,8 @@ import std.stdio;
 import std.string;
 
 import o3o.Xdl;
+import o3o.SEvent;
+import o3o.ULog;
 import o3o.UConfig;
 import derelict.sdl2.sdl;
 
@@ -17,8 +19,12 @@ import derelict.sdl2.sdl;
  *   - 게임루프 등등 게임의 큰 틀을 담당하는 서비스
  */
 class SGame{
-    SDL_Window*   window;
-    SDL_Renderer* renderer;
+    //SDL_Window*   window;
+    //SDL_Renderer* renderer;
+    private XDLScreen oScreen;
+    private SEvent srvcEvent;
+    
+    private SDL_Event oEvent;
 
     /// 생성자
     this(){
@@ -32,30 +38,48 @@ class SGame{
         XDL_Init();
 
         // 게임 창 만들기
-        this.window = XDL_CreateWindow(new UConfig("SGame.cfg"), SDL_WINDOW_SHOWN);
-        
+        //this.window = XDL_CreateWindow(new UConfig("SGame.cfg"), SDL_WINDOW_SHOWN);
         // 게임 렌더러 만들기
-        this.renderer = XDL_CreateRenderer(this.window);
-        
+        //this.renderer = XDL_CreateRenderer(this.window);
+
+        // 오브젝트로 게임 스크린(창/렌더러) 생성
+        this.oScreen = new XDLScreen(
+            new UConfig("SGame.cfg")
+        );
+
+        // 이벤트 서비스
+        this.srvcEvent = new SEvent(
+            this.oScreen
+        );
     }
 
-    // 컨테이너 업데이트
-    void update(int i){
-        XDL_Delay(1000*i);
+    /**
+     * 컴포넌트 모두 업데이트
+     *   - 선언된 주요 컴포넌트들을 모두 업데이트 한다.
+     *   - 업데이트의 우선순위에 주의하도록 한다.
+     */
+    void update(){
+        // 이벤트 업데이트
+        this.srvcEvent.update(oEvent);
     }
 
     // 게임 시작
     void run(){
-        for(int i=0; i<3; i++){
-            this.update(i);
-            writeln("Ho!");
+        // 디버그 메세지
+        debug{
+            ulog("Game Service: start");
         }
-        XDL_Quit();
+
+        // 게임 루프시작
+        while(!this.oScreen.isExit()){
+            // 게임서비스 구동 시작
+            this.update();
+        }
     }
 
     /// 소멸자
     ~this(){
-        this.window = null;
+        //this.window = null;
     }
 }
 
